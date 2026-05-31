3f455fb7d514e5eb572f384e8bd9d23447412887
Rémy CARUHEL <144354787+rCaruhel@users.noreply.github.com>
2026-05-31 19:37:37 +0200

    Ajout de la résolution GLPK, il faut changer le procédé de vérification de chacune des configurations par une heuristique

--- PythonFiles/main.py
+++ PythonFiles/main.py
@@ -1,6 +1,7 @@
 import sys
 import re
 import numpy as np
+import pulp
 
 if len(sys.argv)!=2:
     print("Mauvaises entrées, il faut un unique argument : le chemin vers le fichier d'entrée")
@@ -34,8 +35,13 @@ coveredZones = []
 capteurActuel = 1
 for zonesParCapteur in zonesCouvertesParCapteurs:
     zones = []
+
+    # construction d'un tableau binaire à partir des données de zones couvertes par capteur
     """
-    construction d'un tableau à partir des données de zones couvertes par capteur
+    EXEMPLE : 
+    Les données représentent les zones couvertes par chaque capteur 
+    dans l'exemple suivant, le capteur 1 couvre les zones 1 et 2, le capteur 3 les zones 2 et 3...
+    
     1 2
     2 3
     3
@@ -48,8 +54,9 @@ for zonesParCapteur in zonesCouvertesParCapteurs:
     0 0 1
     1 0 1
     """
-    for i in range (nbrCapteurs-1):
-        if str(i+1) not in zonesParCapteur:
+    zones_list = zonesParCapteur.split()
+    for i in range (nbrZones):
+        if str(i+1) not in zones_list:
             zones.append(0)
         else :
             zones.append(1)
@@ -107,7 +114,58 @@ for element in listes_triees:
     if not est_contenu:
         combinaisonsElementaires.append(element)
 
-print(combinaisonsElementaires)
+#print(combinaisonsElementaires)
 
 ### À présent, on possède les combinaisons élémentaires, on les gardera pour la suite de l'exercice
+'''
+EXEMPLE : 
+Avec les combinaisons élémentaires 
+u0 : [0, 1], 
+u1 : [0, 2], 
+u2 : [0, 3], 
+u3 : [1, 3]
 
+Et les durées de vie des capteurs
+CO : 6
+C1 : 3
+C2 : 2
+C3 : 6
+
+On construit le fichier GLPK pour obtenir le temps max "tui" de chaque zone i, pour maximiser les tui
+
+on maximise tu0 + tu1 + tu2 + tu3
+avec 
+tu0 + tu1 + tu2 <= 6
+tu0 + tu3 <= 3
+tu1 <= 2
+tu2 + tu3 <= 6
+'''
+#### Utilisation de pulp, solveur GLPK
+
+problem = pulp.LpProblem("Maximisation",pulp.LpMaximize)
+variablesTemps = [[] for _ in range(nbrCapteurs)]
+
+
+for i,comb in enumerate(combinaisonsElementaires):
+    for value in comb:
+        variablesTemps[value].append(i)
+
+# Création d'un dictionnaire de variables : t[0], t[1], t[2]...
+# lowBound=0 car un temps ne peut pas être négatif
+t = {u: pulp.LpVariable(f"tu{u}", lowBound=0) for u in range(len(combinaisonsElementaires))}
+
+problem += pulp.lpSum(t[u] for u in range(len(combinaisonsElementaires))), "Maximisation_Temps_Total"
+
+for capteur in range(nbrCapteurs):
+    # On récupère la durée de vie du capteur actuel
+    limite = int(dureesDeVie[capteur])
+    indices_combinaisons = variablesTemps[capteur]
+    contrainte_ligne = pulp.lpSum(t[u] for u in indices_combinaisons)
+
+    # On ajoute la contrainte au problème
+    problem += (contrainte_ligne <= limite, f"Contrainte_Capteur_{capteur}")
+
+problem.solve(pulp.GLPK_CMD(msg=True))
+
+for capteur in range(len(combinaisonsElementaires)):
+    print(f"Capteur{capteur+1} : {t[capteur].varValue}")
